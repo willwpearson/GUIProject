1fa32d36389e0562b064333d946ce39c6d037dc5
willwpearson <31518711+willwpearson@users.noreply.github.com>
2017-11-13 12:58:09 -0700

    Added the spring layout to change the layout of the GUI.

--- src/gui/view/GUIFrame.java
+++ src/gui/view/GUIFrame.java
@@ -6,19 +6,22 @@ import javax.swing.JFrame;
 public class GUIFrame extends JFrame
 {
 	private GUIAppController appController;
+	private GUIPanel appPanel;
 	
 	public GUIFrame(GUIAppController appController)
 	{
 		super();
 		this.appController = appController;
+		this.appPanel = new GUIPanel(appController);
+		
 		setupFrame();
 	}
 	
 	private void setupFrame()
 	{
+		this.setContentPane(appPanel);		
 		this.setTitle("Window title will go here");
 		this.setSize(500, 500);
-		
 		this.setVisible(true);
 	}
 	
--- src/gui/view/GUIPanel.java
+++ src/gui/view/GUIPanel.java
@@ -9,11 +9,43 @@ import javax.swing.SpringLayout;
 public class GUIPanel extends JPanel
 {
 	private GUIAppController appController;
+	private JButton firstButton;
+	private SpringLayout appLayout;
 	
 	public GUIPanel(GUIAppController appController)
 	{
 		super();
 		this.appController = appController;
+		firstButton = new JButton("Click on the button");
+		appLayout = new SpringLayout();
+				
+		setupPanel();
+		setupLayout();
 	}
-
+	
+	/**
+	 * Used to add all components to the screen and install a layout manager.
+	 * Also set color xD
+	 */
+	private void setupPanel()
+	{
+		this.setBackground(Color.DARK_GRAY);
+		this.setLayout(appLayout);
+		this.add(firstButton);
+	}
+	
+	/**
+	 * Used to hold the constraint positioning for SpringLayout.
+	 * AKA a giant dumping ground for all the xxxx.putConstraint(....) lines.
+	 */
+	private void setupLayout()
+	{
+		appLayout.putConstraint(SpringLayout.NORTH, firstButton, 127, SpringLayout.NORTH, this);
+		appLayout.putConstraint(SpringLayout.EAST, firstButton, -139, SpringLayout.EAST, this);
+	}
+	
+	private void setupListeners()
+	{
+		
+	}
 }
